9653207c40c51450f85f82c14388b8c6ad6f8fb8
GanZiB <610699755@qq.com>
2019-12-26 17:04:08 +0800

    first commit

---
+++ src/main/java/com/charles/chatouyu/ChatOuyuApplication.java
@@ -0,0 +1,13 @@
+package com.charles.chatouyu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChatOuyuApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ChatOuyuApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/charles/chatouyu/ChatOuyuApplicationTests.java
@@ -0,0 +1,13 @@
+package com.charles.chatouyu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChatOuyuApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
