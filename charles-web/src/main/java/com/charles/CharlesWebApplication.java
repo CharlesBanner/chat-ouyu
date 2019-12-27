18a68e938eea4358b1e9b39293193596b9272a34
GanZiB <610699755@qq.com>
2019-12-27 11:24:25 +0800

    init

---
+++ charles-web/src/main/java/com/charles/CharlesWebApplication.java
@@ -0,0 +1,13 @@
+package com.charles;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CharlesWebApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CharlesWebApplication.class, args);
+    }
+
+}
