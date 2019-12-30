d547ff75513fa58fc60a99e75096bd1f7b6e0f44
GanZiB <610699755@qq.com>
2019-12-30 16:01:38 +0800

    项目重构bb

--- facade/impl/src/main/java/com/charles/facade/impl/Application.java
+++ facade/impl/src/main/java/com/charles/facade/impl/Application.java
@@ -1,10 +1,12 @@
 package com.charles.facade.impl;
 
+import com.alibaba.nacos.spring.context.annotation.config.NacosPropertySource;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.netflix.feign.EnableFeignClients;
 
+@NacosPropertySource(dataId = "ouyu-server",autoRefreshed = true)
 @SpringBootApplication
 @EnableEurekaClient
 @EnableFeignClients
---
+++ facade/impl/src/main/java/com/charles/facade/impl/web/ConfigController.java
@@ -0,0 +1,28 @@
+package com.charles.facade.impl.web;
+
+import com.alibaba.nacos.api.config.annotation.NacosValue;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+/**
+ * Created with IntelliJ IDEA.
+ * Description:
+ *
+ * @author: GanZiB
+ * Date: 2019-12-30
+ * Time: 10:17
+ */
+@RestController
+@RequestMapping("config")
+public class ConfigController {
+
+    @NacosValue(value = "${useLocalCache:false}", autoRefreshed = true)
+    private boolean useLocalCache;
+
+    @GetMapping("/get")
+    public boolean get() {
+        return useLocalCache;
+    }
+}
